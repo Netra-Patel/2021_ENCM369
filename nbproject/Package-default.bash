06e34ea23a8d4564a26d0cbaf99aa9148c94e514
Netra-Patel <netra.patel@ucalgary.ca>
2021-04-08 13:27:36 -0500

    chnages

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/2021_Encm369.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=2021_Encm369.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=2021encm369/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PICActivity_7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=PICActivity_7.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=picactivity7.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/2021encm369/bin
+makeDirectory ${TMPDIR}/picactivity7.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/2021encm369.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/picactivity7.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/2021encm369.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/picactivity7.x.tar *
 checkReturnCode
 
 # Cleanup
